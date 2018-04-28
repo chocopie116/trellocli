fd3d79e8d6220886508a7d02c92918fcf246a884
chocopie116 <chocopie116@gmail.com>
2018-04-28 22:36:12 +0900

    APIリクエストするようにした

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # trellocli
+
+generate Developer API Keys from following URL.
+https://trello.com/app-key
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/urfave/cli"
 	"log"
 	"os"
+
+	"github.com/urfave/cli"
+	"github.com/adlio/trello"
 )
 
 var logger = log.New(os.Stdout, "", log.Ldate+log.Ltime+log.Lshortfile)
@@ -16,20 +18,42 @@ var Commands = []cli.Command{
 				Name:  "board",
 				Usage: "Target board",
 			},
+			cli.StringFlag{
+				Name:  "app_key",
+				Usage: "Trello API app key",
+			},
+			cli.StringFlag{
+				Name:  "token",
+				Usage: "Trello API token",
+			},
 		},
 		Action: list,
 	},
 }
 
+
+
 func main() {
 	app := cli.NewApp()
 	app.Commands = Commands
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name: "app_key",
+		},
+	}
 	app.Run(os.Args)
 }
 
 func list(c *cli.Context) error {
+	k := c.String("app_key")
+	t := c.String("token")
+
+	client := trello.NewClient(k, t)
+
 	b := c.String("board")
-	logger.Print(b)
+	bc, _ := client.GetBoard(b, trello.Defaults())
+
+	logger.Print(bc)
 
 	return nil
 }
