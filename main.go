f4a90ac2bd97e3a0d059a63a02fadaa8d168abd6
chocopie116 <chocopie116@gmail.com>
2018-04-29 01:23:35 +0900

    Cardつくれるようにした

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/adlio/trello"
+	"github.com/k0kubun/pp"
 	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 )
@@ -31,30 +32,53 @@ var commands = []cli.Command{
 		},
 		Action: list,
 	},
+	cli.Command{
+		Name: "add",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "board",
+				Usage: "Target board",
+			},
+			cli.StringFlag{
+				Name:  "app_key",
+				Usage: "Trello API app key",
+			},
+			cli.StringFlag{
+				Name:  "token",
+				Usage: "Trello API token",
+			},
+			cli.StringFlag{
+				Name:  "card_name",
+				Usage: "card name",
+			},
+		},
+		Action: add,
+	},
 }
 
 func main() {
 	app := cli.NewApp()
 	app.Commands = commands
-	app.Flags = []cli.Flag{
-
-		cli.StringFlag{
-			Name: "app_key",
-		},
-	}
 	app.Run(os.Args)
 }
 
-func list(c *cli.Context) error {
-	k := c.String("app_key")
-	t := c.String("token")
+func initClient(k string, t string) *trello.Client {
 	if k == "" || t == "" {
-		logger.Fatalln("app_key and token is requred.")
+		logger.Fatalln("app_key and token is requred parameter.")
 	}
 
 	client := trello.NewClient(k, t)
 
+	return client
+}
+
+func list(c *cli.Context) error {
+	client := initClient(c.String("app_key"), c.String("token"))
+
 	b := c.String("board")
+	if b == "" {
+		logger.Fatalln("board is required parameter")
+	}
 	board, err := client.GetBoard(b, trello.Defaults())
 	if err != nil {
 		logger.Fatalln(errors.Wrap(err, fmt.Sprintf("failed Get Board by ID. Value: %#v", b)))
@@ -68,6 +92,7 @@ func list(c *cli.Context) error {
 	fmt.Println(time.Now().Format("2006/01/02"))
 	fmt.Println("```")
 	for _, list := range lists {
+		pp.Print(list.ID)
 		if list.Name == "TODO" {
 			continue
 		}
@@ -84,6 +109,26 @@ func list(c *cli.Context) error {
 		fmt.Print("\n")
 	}
 	fmt.Println("```")
+
+	return nil
+}
+
+func add(c *cli.Context) error {
+	client := initClient(c.String("app_key"), c.String("token"))
+	cn := c.String("card_name")
+	if cn == "" {
+		logger.Fatalln("card_name is required parameter")
+	}
+
+	card := trello.Card{
+		Name:   cn,
+		IDList: "59aabd6963041edd45105f05", //TODO ハードコーディングやめる
+	}
+
+	err := client.CreateCard(&card, trello.Defaults())
+	if err != nil {
+		logger.Fatalln(errors.Wrap(err, "failed create Cards on List"))
+	}
 
 	return nil
 }
