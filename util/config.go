4ae195cab93abf48fe9b298fdb12ca2470a40747
chocopie116 <chocopie116@gmail.com>
2018-04-29 22:31:55 +0900

    remove unused toml value

--- util/config.go
+++ util/config.go
@@ -15,7 +15,6 @@ type ApiConfig struct {
 }
 type TargetConfig struct {
 	BoardId       string   `toml:"board_id"`
-	AddListId     string   `toml:"add_list_id"`
 	ShowListNames []string `toml:"show_list_names"`
 	AddListName   string   `toml:"add_list_name"`
 }
